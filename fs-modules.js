c6f7a58acb0d1480249acb4069620e3f0bc6afef
Dani <dan.avila7@gmail.com>
2022-11-12 15:50:57 -0500

    read and write

--- fs-modules.js
+++ fs-modules.js
@@ -2,3 +2,5 @@ const { readFileSync, writeFileSync } = require('fs');
 
 const reados = readFileSync('./os.js', 'utf8');
 console.log(reados);
+
+writeFileSync('./text1.txt', `test de texto creado por node`);
